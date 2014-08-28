2f7f3d307d49a677b8e380c2270b675a5bc9cfd2
dyu-brownsys <da_yu@brown.edu>
2014-08-27 21:55:20 -0400

    First Commit: Finish Floyd_Warshall Algorithm

---
+++ route.thrift
@@ -0,0 +1,22 @@
+struct Query
+{
+    1: required list<string> arguments
+}
+
+struct QueryReply
+{
+    1: required list<list<string>> result,
+    2: optional string exception_code,
+    3: optional string exception_message
+}
+
+struct Notification
+{
+    1: required string notificationType,
+    2: required map<string, string> values
+}
+
+service RouteService{
+    QueryReply doQuery(1:Query request),
+    void notifyMe(1:Notification notify)
+}
---
+++ thriftClient.py
@@ -0,0 +1,58 @@
+import sys
+sys.path.append('./gen-py')
+ 
+from route import RouteService
+from route.ttypes import *
+
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+from thrift.server import TServer
+from thrift import Thrift
+ 
+import socket
+
+try:
+    transport = TSocket.TSocket('192.168.1.103', 9090)
+    transport = TTransport.TBufferedTransport(transport)
+    protocol = TBinaryProtocol.TBinaryProtocol(transport)
+    client = RouteService.Client(protocol)
+    transport.open()
+
+    #Test topology
+    f = open("test_topo", "r")
+
+    for line in f.readlines()[1:]:
+        line = line.strip()
+        tokens = line.split(",")
+        n = Notification()
+        n.notificationType = "LINKSTATE"
+        n.values = {}
+        n.values["srcsw"] = tokens[0]
+        n.values["srcpt"] = tokens[1]
+        n.values["dstsw"] = tokens[2]
+        n.values["dstpt"] = tokens[3]
+        n.values["cost"] = tokens[4]
+        client.notifyMe(n)
+    f.close()   
+    
+    f = open("switch_config", "r")
+
+    for line in f.readlines()[1:]:
+        line = line.strip()
+        token = line.split(",")
+        n = Notification()
+        n.notificationType = "SWITCH_CONFIG"
+        n.values = {}
+        n.values["swid"] = token[0]
+        n.values["ptid"] = token[1]
+        n.values["prefix"] = token[2]
+        n.values["mask"] = token[3]
+        client.notifyMe(n)
+    f.close()
+    
+    q = Query()
+    client.doQuery(q)
+
+except Thrift.TException, ex:
+  print "%s" % (ex.message)
